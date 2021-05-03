07ebc5c7a66e39ed8a8ee3673bf9a4848901753e
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-05-03 15:49:51 +0200

    inizio progetto

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    $parText = 'lorem ipsum ecc ecc ecc lorem ipsum ecc ecc ecc lorem ipsum ecc ecc ecc lorem ipsum ecc ecc ecc';    
+    
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Badword</title>
+</head>
+<body>
+    <p><?php echo $parText; ?></p>
+    <p>Il paragrafo precedente ha lunghezza uguale a: <?php echo strlen($parText); ?></p>
+   
+</body>
+</html>
